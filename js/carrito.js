73b4dfa1201ad7cbfd5bf032f1f12ebe29bce99f
Daniela Mousalli <dani.moussalli@gmail.com>
2021-08-08 18:45:26 -0300

    agrego archivo para carrito.js

---
+++ js/carrito.js
@@ -0,0 +1,77 @@
+//SIMULADOR INTERACTIVO
+//CALCULAR EL COSTO DE UN PRODUCTO
+
+
+//CREO UNA FUNCION PARA CALCULAR EL VALOR FINAL DE LA COMPRA
+const calcularTotal = function (cantidad, precioUnitario){
+    let total = cantidad * precioUnitario;
+    return total;
+}
+
+// --- Pido que ingrese los valores de cantidad y precio para poder calcular el valor final
+let cantidad = parseInt(prompt("Ingrese la cantidad"));
+let precioUnitario = parseInt(prompt("Ingrese precio"));
+
+// --- Calculo el valor total a pagar 
+let total = calcularTotal (cantidad, precioUnitario);
+console.log("El precio a pagar es: " + total);
+// FIN FUNCION
+
+
+
+
+//INGRESO CUPON DE DESCUENTO
+function cuponDescuento (codigo){
+    if (codigo == "PRIMERACOMPRA"){
+        // calculo el valor final con descuento
+        let calculoDescuento = (total * 10 ) / 100;
+        let totalConDescuento = total - calculoDescuento
+        // verifico que este bien hecho el calculo
+        console.log(totalConDescuento); 
+        return totalConDescuento;
+    }else{
+        // asigno el valor final sin descuento
+        totalConDescuento = total;
+        return totalConDescuento;
+    }
+}
+
+// --- Solicitò el nombre del cupon
+let codigo = prompt("Ingrese codigo de descuento");
+// --- Paso el codigo a mayuscula
+codigo = codigo.toLocaleUpperCase();
+// --- Veo que codigo fue el ingresado
+console.log("Codigo Ingresado: " + codigo);
+// --- Llamo a la funcion
+let totalFinal = cuponDescuento(codigo);
+
+// --- Depende el codigo ingresado es el mensaje a mostrar
+if (codigo == "PRIMERACOMPRA"){
+    alert("El precio total con descuento es " + totalFinal);
+} else{
+    alert("El precio total sin descuento es " + totalFinal);
+}
+// FIN CUPON
+
+
+
+
+
+//ARMADO DE OBJETO PARA ARMAR CADA PRODUCTO
+function Producto (nombre, precio){
+    this.nombre = nombre;
+    this.precio = precio;
+}
+
+// --- Paso los parametros para crear el objeto 
+const ipa = new Producto ("IPA", 200);
+const bock = new Producto ("Bock", 250);
+const paleAle = new Producto ("Pale Ale", 200);
+console.log(ipa);
+console.log(bock);
+console.log(paleAle);
+// FIN OBJETO
+
+
+
+
